1d759905e4b6db209ef83f1c32a22ebc9e554717
Joey <jtroinacoding@gmail.com>
2022-01-01 23:32:16 -0500

    Initial Commit

---
+++ Shared/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  Shared
+//
+//  Created by admin on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
